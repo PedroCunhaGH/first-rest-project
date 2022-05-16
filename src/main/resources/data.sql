e69a291b521892302ca410ed6eaaf07919b0f011
brunospcastro <brunoscastro@ua.pt>
2022-05-16 15:20:58 +0100

    .sql: fixed

--- src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `person` (`name`,`role`,`pwd`) VALUES (' Bruno','USER','1234');
--- src/main/resources/db_script.sql
+++ src/main/resources/db_script.sql
@@ -2,9 +2,9 @@ create database sports;
 use sports;
 
 CREATE TABLE IF NOT EXISTS `person` (
-  `personID` int NOT NULL, AUTO_INCREMENT,
+  `personID` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `role` varchar(100) NOT NULL,
   `pwd` varchar(100) NOT NULL,
-  PRIMARY KEY (`person_id`)
+  PRIMARY KEY (`personID`)
 );
