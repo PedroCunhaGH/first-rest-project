e9a4163a64be1aac3094835fc4e018a6d3e68c5d
PEDRO CUNHA <pedro.cunha@theloop.pt>
2022-05-19 16:13:33 +0100

    13. StandingsRestController Changes - minor

--- src/main/java/com/example/API/Reader/rest/StandingsRestController.java
+++ src/main/java/com/example/API/Reader/rest/StandingsRestController.java
@@ -1,11 +1,10 @@
 package com.example.API.Reader.rest;
 
-
-import com.example.API.Reader.model.League;
 import com.example.API.Reader.model.Standings;
 import com.example.API.Reader.proxy.StandingsProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,7 +14,7 @@ public class StandingsRestController {
     @Autowired
     StandingsProxy standingsProxy;
 
-    @GetMapping("/{id}/standings?season={year}&sort=asc")
+    @GetMapping("{id}/standings?season={year}&sort=asc")
     @ResponseBody
     public Standings getStanding(@PathVariable("id") String id, @RequestParam("year") Integer year) {
         return standingsProxy.getStanding(id, year);
